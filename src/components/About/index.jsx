bf9c402bfd794b628cd5f622694edab96bab0ccc
Opu <opuofficial24@gmail.com>
2023-08-19 01:29:26 +0600

    Updated 'About Me' section

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -7,22 +7,25 @@ function About() {
       <h3 data-heading="Self-Taught Developer">About Me</h3>
 
       <p>
-        I am an enthusiastic junior MERN stack developer, driven by my passion
-        to create delightful and intuitive web experiences. Embracing the
-        ever-evolving field of web development, I am eager to continuously learn
-        and grow in this exciting domain. With a strong passion for technology,
-        I love exploring web development and staying up-to-date with the latest
-        tools and techniques for great-looking websites.
+        Passionate Frontend Developer with solid knowledge of React, dedicated
+        to crafting dynamic user experiences. Proficient in HTML, CSS, and
+        JavaScript, I am committed to continuous learning and growth in this
+        exciting field. I prioritize staying up-to-date with the latest tools
+        and techniques for creating outstanding websites.
       </p>
       <p>
-        Thank you for considering me as part of your team. I'm eager to
-        collaborate, absorb knowledge, and build meaningful web solutions that
-        positively impact users' lives. Let's create something incredible
-        together!
+        I'm eager to collaborate, absorb knowledge, and contribute to the
+        development of visually stunning and user-centric frontend solutions.
+        Let's work together to create web interfaces that captivate and delight
+        users at every interaction!
       </p>
 
       <div className="view__resume">
-        <a href="https://drive.google.com/file/d/1wt7d5w-6LtL8kqsICBJzkP1COF-nHshb/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://drive.google.com/file/d/1wt7d5w-6LtL8kqsICBJzkP1COF-nHshb/view?usp=sharing"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <span>View Resume</span>
           <i className="fa-solid fa-arrow-right"></i>
         </a>
